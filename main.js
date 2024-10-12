4a89ed18851196be3063d58ca96bed53e197988a
c8e4 <c8e4@proton.me>
2024-10-12 15:09:16 +0200

    walls+corners

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { init } from './src/init';
 import { onResize } from './src/onResize';
 
 // initial setup
-const viewSize = 10;
+const viewSize = 15;
 const { scene, renderer, camera, controls } = init(TR, viewSize);
 setLight(TR, scene);
 window.addEventListener('resize', () => onResize());
@@ -20,25 +20,34 @@ animate();
 let blocks = await loadRetroForrest(TR);
 createLevel();
 
-export function set(blockName, x = 0, y = 0) {
-  const b = blocks[blockName].clone();
-  b.position.set(x, 0, y);
-  scene.add(b);
-}
-
 function randomFloor() {
   const x = ['floor_A', 'floor_B', 'floor_C'];
   const r = Math.floor(Math.random() * 3);
   return x[r];
 }
 
+export function set(blockName, x = 0, y = 0, rx = 0) {
+  const b = blocks[blockName].clone();
+  b.rotation.y = rx;
+  b.position.set(x, 0, y);
+  scene.add(b);
+}
+
 export async function createLevel() {
   const s = 10;
   for (let a = 0; a < s; a++) {
     set('wall_A', a - s / 2, -s / 2 - 1.5);
+    set('wall_A', a - s / 2, s / 2 + 0.5, Math.PI);
+    set('wall_A', s / 2 + 0.5, a - s / 2, -Math.PI / 2);
+    set('wall_A', -s / 2 - 1.5, a - s / 2, Math.PI / 2);
     for (let b = 0; b < s; b++) {
       set(randomFloor(), a - s / 2, b - s / 2);
     }
   }
+  set('wall_cornerInCap_AB', -s / 2 - 1.5, -s / 2 - 1.5);
+  set('wall_cornerInCap_AB', s / 2 + 0.5, s / 2 + 0.5, -Math.PI);
+  set('wall_cornerInCap_AB', -s / 2 - 1.5, s / 2 + 0.5, Math.PI / 2);
+  set('wall_cornerInCap_AB', s / 2 + 0.5, -s / 2 - 1.5, -Math.PI / 2);
+
   //set('floor_big', 1, 1);
 }
