874f1cf31d25cfff1f67a42feb2c1f785251cddf
c8e4 <c8e4@proton.me>
2024-10-13 01:24:31 +0200

    improve code

--- src/loadBuildings.js
+++ src/loadBuildings.js
@@ -1,47 +1,40 @@
-import { FBXLoader, GLTFLoader } from 'three/examples/jsm/Addons.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { TGALoader } from 'three/examples/jsm/loaders/TGALoader.js';
 import { buildings } from './buildings.js';
 
 const FBX_PATH = './assets/TT_RTS_Standard/models/buildings/';
 const TEXTURE_PATH = './assets/TT_RTS_Standard/models/materials/';
 
-export async function loadBuildings(TR) {
+export async function loadBuildings() {
   const gltfLoader = new GLTFLoader();
-  const textureLoader = new TR.TextureLoader();
+  const textureLoader = new TGALoader();
 
   const blocks = {};
 
   for (let i = 0; i < buildings.length; i++) {
     let model = await gltfLoader.loadAsync(FBX_PATH + buildings[i]);
 
-    let textureName = `TT_RTS_Buildings_texture.tga`;
+    let textureName = 'TT_RTS_Buildings_texture.tga';
 
     const texture = textureLoader.load(TEXTURE_PATH + textureName);
-    texture.colorSpace = TR.SRGBColorSpace;
-    texture.magFilter = TR.NearestFilter;
-    texture.minFilter = TR.NearestFilter;
+    texture.colorSpace = THREE.SRGBColorSpace;
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
 
     const objectToTraverse = model.scene || model;
-    objectToTraverse.traverse(applyTexture);
-
-    function applyTexture(child) {
-      if (child.isMesh && child.material) {
-        if (child.material.map) {
-          child.material.map = texture;
-          if (child.material.map.repeat) {
-            child.material.map.repeat.set(1, 1);
-          }
-          if (child.material.map.offset) {
-            child.material.map.offset.set(0.0, 0.0);
-          }
-          child.material.needsUpdate = true;
-        }
+    objectToTraverse.traverse(function (child) {
+      if (child.isMesh) {
+        child.material = new THREE.MeshStandardMaterial({
+          map: texture,
+        });
+        child.material.needsUpdate = true;
       }
-    }
+    });
 
     const targetObject = model.scene || model;
     targetObject.position.set(0, 0, 0);
 
-    //blocks[buildings[i].split('.')[0]] = targetObject;
     blocks[buildings[i].split('.')[0]] = targetObject;
   }
 
